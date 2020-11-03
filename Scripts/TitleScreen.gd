1cf23bd830cf2e2bb1952f915aafd518474997fc
rkpela <rkpela@gmail.com>
2020-11-02 21:54:28 -0500

    Main menu is done now need to start working on game

---
+++ Scripts/TitleScreen.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var scene_path_to_load
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Menu/CenterRow/Buttons/StartGame.grab_focus()
+	# Need to change Options button to have a different scene to load
+	for button in $Menu/CenterRow/Buttons.get_children():
+		print(button)
+		button.connect("pressed",self,"on_Button_pressed",[button.scene_to_load])
+	pass # Replace with function body.
+
+func on_Button_pressed(scene_to_load):
+	scene_path_to_load = scene_to_load
+	$FadeIn.show()
+	$FadeIn.fade_in()
+	
+func _on_FadeIn_fade_finished():
+	if(scene_path_to_load == "EXIT"):
+		get_tree().quit()
+	else:
+		get_tree().change_scene(scene_path_to_load)
+	pass # Replace with function body.
