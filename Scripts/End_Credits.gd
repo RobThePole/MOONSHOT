a52aac72ebbbaddf10312e8e39ea845d2c8193cc
rkpela <rkpela@gmail.com>
2020-11-08 21:52:03 -0500

    Added End_Credits to game tried adding pause menu but did not work well with my camera settings

---
+++ Scripts/End_Credits.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	pass
+	
+	
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://Scene/menu/TitleScreen.tscn")
